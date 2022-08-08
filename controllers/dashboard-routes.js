06a4a8fdfdacbe7c00ff5f57c0d34b6a061b7f36
kfisch2 <kmlindsey24@gmail.com>
2022-08-08 14:55:08 -0700

    edit post route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -47,4 +47,44 @@ router.get('/', withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// GET route for editing posts
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'Post with that id does not exist' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      res.render('edit-post', {
+        post,
+        loggedIn: true,
+      });
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
 module.exports = router;
